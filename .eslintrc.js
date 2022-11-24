aac132b3cac100cd548412d5bafdacb6306a0eab
J. Ryan Rembert <jrrembert@users.noreply.github.com>
2022-11-24 15:46:26 -0800

    feat: setup initial eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+const rules = {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "no-constant-condition": "off",
+    "no-redeclare": "off",
+    "no-var": "off",
+    "prefer-const": "off",
+  };
+  
+  module.exports = {
+    root: true,
+    ignorePatterns: ["**/dist/**"],
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: "module",
+    },
+    env: { es6: true },
+    rules,
+  
+    overrides: [
+      {
+        files: ["packages/*/types.ts"],
+        rules: {
+          // these are meant to test the typescript typings, unused variables are expected
+          "@typescript-eslint/no-unused-vars": "off",
+        },
+      },
+      {
+        files: [
+          ".eslintrc.js",
+          "packages/*/bench.js",
+          "packages/*/test.js",
+          "packages/turf/rollup.config.js",
+          "scripts/check-dependencies.js",
+        ],
+        env: {
+          node: true,
+        },
+      },
+    ],
+  };
