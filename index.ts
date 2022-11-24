a5402a09d3cba8086029a8e03e7c711c17e2df21
J. Ryan Rembert <jrrembert@users.noreply.github.com>
2022-11-24 15:28:42 -0800

    feat: add generate command and tests

---
+++ index.ts
@@ -0,0 +1,42 @@
+function generate(value: string): string {
+    console.log(value);
+
+    if (!value.length) {
+        throw new Error('string cannot be empty');
+    }
+
+    const test = 'test'
+    // convert to array
+    const toArray = Array.from(value);
+
+    // if double is true, multiply digit by 2
+    let double = true;
+
+    // iterate through value, multiplying value by 2 for every 2nd digit
+    const sum = toArray.reduceRight((prev, current) => {
+        if (double === true) {
+            double = false
+            const temp = parseInt(current) * 2;
+
+            // if value is greater than or equal to 10, sum each digit ie. if value is 15, use 1 + 5 to get value
+            if (temp >= 10) {
+                return prev + Array.from(temp.toString()).reduce((prev, current) => {
+                    return prev + parseInt(current)
+                }, 0)
+            }
+
+            return prev + parseInt(current) * 2
+        } else {
+            double = true;
+            return prev + parseInt(current)
+        }
+    }, 0);
+
+    const checkDigit = (10 - (sum % 10)) % 10;
+
+    return value.concat(checkDigit.toString());
+}
+
+export default {
+    generate
+}
